4bd9b7b83a9612f546e9c03bd838bdb51beea574
Elmar <elmar.bruckmann@outlook.de>
2018-11-18 02:03:06 +0100

    implementing lagazy support. almost finished.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
 rename = require('gulp-rename'),
-del = require('del');
+del = require('del'),
+svg2png = require('gulp-svg2png');
 
 var config = {
   mode: {
     css: {
+      variables: {
+        replaceSvgWithPng: function() {
+          return function(sprite, render) {
+            return render(sprite).split('.svg').join('.png');
+          }
+        }
+      },
       sprite: 'sprite.svg',
       render: {
         css: {
@@ -20,14 +28,20 @@ gulp.task('beginClean', () => {
   return del(['./app/temp/sprite', './app/assets/images/sprites']);
 });
 
-gulp.task('createSprite', ['beginClean'], () =>{
+gulp.task('createSprite', ['beginClean'], () => {
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite'));
 });
 
-gulp.task('copySpriteGraphic', ['createSprite'], () => {
-  return gulp.src('./app/temp/sprite/css/**/*.svg')
+gulp.task('createPNG', ['createSprite'], () => {
+  return gulp.src('./app/temp/sprite/css/*.svg')
+    .pipe(svg2png())
+    .pipe(gulp.dest('./app/temp/sprite/css'));
+});
+
+gulp.task('copySpriteGraphic', ['createPNG'], () => {
+  return gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
     .pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
@@ -41,4 +55,4 @@ gulp.task('endClean', ['copySpriteGraphic', 'copySprite'], () => {
   return del(['./app/temp/sprite']);
 });
 
-gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySprite', 'endClean']);
+gulp.task('icons', ['beginClean', 'createSprite', 'createPNG', 'copySpriteGraphic', 'copySprite', 'endClean']);
